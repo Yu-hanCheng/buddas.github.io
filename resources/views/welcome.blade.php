7c874528e8d26adf629593f7c154808102223e88
Yu-hanCheng <sarahcheng1231@gmail.com>
2021-05-17 08:04:05 +0800

    INIT

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,92 @@
+
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>發一崇德台南道場</title>
+<meta name="keywords" content="發一崇德台南道場,發一崇德,台南道場,台南" />
+<meta name="description" content="發一崇德台南道場，響應您我善願同步誦經祈願，恭誦彌勒救苦真經，心經
+誦經後點擊「我已經完成誦經」迴向，把愛串起來 凝聚您我慈悲之心，叩求諸天仙佛慈悲挽化疫情止息 願天下眾生平安" />
+
+<meta property="og:title" content="發一崇德台南道場,發一崇德,台南道場,台南" /> 
+<meta property="og:description" content="發一崇德台南道場，響應您我善願同步誦經祈願，恭誦彌勒救苦真經，心經
+誦經後點擊「我已經完成誦經」迴向，把愛串起來 凝聚您我慈悲之心，叩求諸天仙佛慈悲挽化疫情止息 願天下眾生平安" />
+<style type="text/css">
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-image: url();
+	background-color: #000;
+}
+</style>
+<link href="kh2020.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript">
+function MM_goToURL() { //v3.0
+  var i, args=MM_goToURL.arguments; document.MM_returnValue = false;
+  for (i=0; i<(args.length-1); i+=2) eval(args[i]+".location='"+args[i+1]+"'");
+}
+</script>
+</head>
+
+<body>
+<div width="100%" >  
+<table width="90%" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+      <td width="30%" align="center" valign="middle" class="T5"><a href="http://www.cdkh.org.tw" title="發一崇德台南道場" target="_top"><img src="../images/logo.png" alt="" width="200" height="134" /></a></td>
+      <td width="70%" height="150" align="left" valign="middle" class="T5">發一崇德台南學界</td>
+    </tr>
+    <tr>
+      <td colspan="2" align="center" valign="middle"><img src="{{ URL::to('/') }}/image/command.jpeg"  width="561" /></td>
+    </tr>
+    <tr>
+      <td height="500" colspan="2" align="center" valign="middle"><span class="T1">發一崇德台南道場<br />
+響應您我善願同步祈願<br />
+
+愛的共振<br />
+感謝台灣比起世界<br />
+晚了一年才擴散<br />
+
+台灣疫情擴散<br />
+<span class="T2">我們一起用正能量轉化負能量</span><br />
+相信上天、相信愿力、相信愿力抵業力<br />
+事事感恩遇考懺悔、希望人人都能脫劫關<br />
+並培養蔬食與誦經祈福的習慣<br />
+
+#善願同步迴向祈願#<br />
+下面三類其一完成後點擊<span class="T2">「我已經完成」迴向</span><<br />
+把愛串起來 凝聚您我慈悲之心<br />
+叩求諸天仙佛慈悲<br />
+挽化疫情止息 願天下眾生平安<br />
+
+    </tr>
+    <tr>
+      <td height="200" colspan="2" align="center" valign="middle"><table width="80%" border="0" align="center" cellpadding="0" cellspacing="0">
+      <tr>
+          <td width="30%" align="center"><a><font size="5" color="#00ff00" >彌勒救苦真經 --1萬5千部迴向</font></a></td>
+          <td width="30%" align="center"><a name="button4" id="button4_" value="誠心叩首祈願祝禱--300萬叩頭迴向"></a> </td>
+          <td width="30%" align="center"><a name="button3"id="button3_" value="蔬醒行動人人響應--1日1齋疫情消災"></a> </td>
+        </tr>  
+      <tr>
+          <td width="30%" align="center"><input name="button99" type="submit" class="btn btn-primary"  id="button99" value="我已經完成誦經1部/次"/></td>
+          <td width="30%" align="center"><input name="button4" type="submit" class="btn btn-danger"  id="button4" value="我已經完成叩首1000下/次" /></td>
+          <td width="30%" align="center"><input name="button3" type="submit" class="T4 btn btn-success" id="button3" value="我已經響應吃素1餐/次" /></td>
+        </tr>
+   <tr>
+      <td width="30%" height="30"  align="center" valign="middle" style="background:azure">誦經累計次數：<span >111</span>次</td>
+      <td width="30%" height="30"  align="center" valign="middle" style="background:azure">叩首累計次數：<span >8476</span>次</td>
+      <td width="30%" height="30"  align="center" valign="middle" style="background:azure">一日一齋累計次數：<span >8476</span>次</td>
+    </tr>
+    <tr>
+      <td colspan="2" align="center" valign="middle" class="T2">&nbsp;</td>
+    </tr>
+  </table>
+  &gt;&gt;
+  <table width="90%" border="0" align="center" cellpadding="0" cellspacing="0"><tr>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
